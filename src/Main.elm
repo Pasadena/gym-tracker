e6e2e47913af0afdbcc1dac30db10a9b2566e670
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2018-03-07 21:22:42 +0200

    Initial commit with some working stuff

---
+++ src/Main.elm
@@ -0,0 +1,235 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, h3, img, button, label, input, select, option, span, p, h4)
+import Html.Attributes exposing (src, class, value, type_, style)
+import Html.Events exposing (onClick, onInput)
+
+import Date exposing(..)
+import Task exposing(..)
+import Date.Extra
+
+
+---- MODEL ----
+
+type alias Rep =
+  {
+  weight: Int
+  , repetitions: Int
+  }
+
+type alias ExerciseType =
+  { name: String }
+
+type alias Exercise =
+  {
+  exerciseType: String
+  , sets: List Rep
+  }
+
+type alias Workout =
+  { date: Date
+  , weekNumber: Int
+  , exercises: List Exercise
+  }
+
+type alias Model =
+  {
+    workouts: List Workout,
+    currentWorkout: Maybe Workout,
+    currentDate: Maybe Date,
+    currentExercise: Exercise,
+    selectedExercise: String,
+    repWeight: String,
+    repetitions: String
+  }
+
+model: Model
+model =
+  {
+  workouts = []
+  , currentWorkout = Nothing
+  , currentDate = Nothing
+  , currentExercise = currentExercise
+  , selectedExercise = ""
+  , repWeight = ""
+  , repetitions = ""
+  }
+
+currentExercise: Exercise
+currentExercise =
+  {
+    exerciseType = "",
+    sets = []
+  }
+
+defaultTypes: List ExerciseType
+defaultTypes =
+  { name = "Bench press" } :: { name = "Deadlift" } :: { name = "Incline Dumbbell Press" }  :: []
+
+now : Cmd Msg
+now =
+  Task.perform (Just >> Current_Date) Date.now
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, now )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = Init_Workout
+    | Current_Date (Maybe Date)
+    | Exercise_Selected String
+    | Weight_Changed String
+    | Repetitions_Changed String
+    | Add_Set
+    | Add_Exercise
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+      Init_Workout ->
+        let
+          newModel = startNewWorkout model
+        in
+          (newModel, Cmd.none)
+      Current_Date date ->
+        let
+          nextModel =
+            { model | currentDate = date }
+        in
+          (nextModel, Cmd.none)
+      Exercise_Selected exerciseName ->
+        ({ model | selectedExercise = exerciseName }, Cmd.none)
+      Weight_Changed weight ->
+        ({ model | repWeight = weight }, Cmd.none)
+      Repetitions_Changed reps ->
+        ({ model | repetitions = reps }, Cmd.none)
+      Add_Set ->
+        let
+          newSet = { weight = Result.withDefault 0 (String.toInt model.repWeight)
+            , repetitions = Result.withDefault 0 (String.toInt model.repetitions) }
+          withNewSet = newSet :: model.currentExercise.sets
+          nextModel = { model | currentExercise = model.currentExercise |> setListOfSets withNewSet }
+        in
+          (nextModel, Cmd.none)
+      Add_Exercise ->
+        let
+          existingExercises = case model.currentWorkout of
+            Just workout -> workout.exercises
+            Nothing -> []
+          withNewExercise = model.currentExercise :: existingExercises
+          nextModel = { model | currentWorkout = model.currentWorkout |> setListOfExercises withNewExercise }
+        in
+          (nextModel, Cmd.none)
+
+
+setListOfSets: List Rep -> Exercise -> Exercise
+setListOfSets sets exercise =
+  { exercise | sets = sets }
+
+setListOfExercises: List Exercise -> Maybe Workout -> Maybe Workout
+setListOfExercises exercises workout =
+  case workout of
+    Just wo -> Just { wo | exercises = exercises }
+    Nothing -> Nothing
+
+
+startNewWorkout: Model -> Model
+startNewWorkout model =
+  let
+    today = case model.currentDate of
+      Nothing -> Date.fromString "2018/1/1" |> Result.withDefault (Date.fromTime 0)
+      Just date -> date
+    newWorkout =
+      { date = today
+      , weekNumber = Date.Extra.weekNumber today
+      , exercises = []
+      }
+    newModel = { model | currentWorkout = Just newWorkout }
+  in
+    newModel
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div [class "content"]
+        [
+        h3 [] [ text "Current week"]
+        , div [class "week-container"] [  ]
+        , button [ onClick Init_Workout ] [ text "New exercise"]
+        , workoutForm model
+      ]
+
+workoutForm: Model -> Html Msg
+workoutForm model =
+  case model.currentWorkout of
+    Nothing -> div [] []
+    Just workout ->
+      div [ class "workout-form" ] [
+        div [ class "workout-form-header" ] [
+          h3 [] [ text "Create a new workout"]
+        ]
+        , div [ class "workout-form-content" ] [
+          div [ class "input-container" ] [
+            label [] [ text "Whatcha gonna do:"]
+            , select [ class "type-select", value model.selectedExercise, onInput Exercise_Selected ] ( List.map typeOption defaultTypes )
+          ]
+          , newRepItem
+          , div [] ( List.map repItem model.currentExercise.sets )
+          , div [] [
+            button [ onClick Add_Exercise] [ text "Add Exercise"]
+          ]
+        ]
+        , div [ class "workout-form-footer" ] [
+          button [ class "button-primary" ] [ text "Save" ]
+          , button [ class "button" ] [ text "Cancel" ]
+        ]
+      ]
+
+typeOption: ExerciseType -> Html Msg
+typeOption exerciseType =
+  option [ value exerciseType.name ] [ text exerciseType.name ]
+
+repItem: Rep -> Html Msg
+repItem rep =
+  div [ class "rep-item" ] [
+    p [ class "rep-item-val" ] [ text (String.append (toString rep.repetitions) " reps ") ]
+    , span [ style [("alignSelf", "center"), ("marginLeft", "3px"), ("marginRight", "3px")] ] [ text "w/" ]
+    , p [ class "rep-item-val" ] [ text (String.append (toString rep.weight) " kg") ]
+  ]
+
+newRepItem: Html Msg
+newRepItem =
+  div [ class "rep-item" ] [
+    div [ class "input-container" ] [
+      label [ class "input-label" ] [ text "Repetitions" ]
+      , input [ type_ "text", onInput Repetitions_Changed ] []
+    ]
+    , div [ class "input-container" ] [
+      label [ class "input-label" ] [ text "Weight" ]
+      , input [ type_ "text", onInput Weight_Changed ] []
+    ]
+    , button [ onClick Add_Set  ] [ text "Add to Exercise"]
+  ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
