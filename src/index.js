bbb2a99b364b959f2d228d37d25ac15601d6acac
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2018-03-26 23:12:21 +0300

    Added firebase to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
+import fbConfig from '../firebase-config';
 
 Main.embed(document.getElementById('root'));
 
 registerServiceWorker();
+
+firebase.initializeApp(fbConfig);
+console.log(fbConfig);
